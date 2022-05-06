dafe1358c1d95f24e600a713c279a371f530363a
Rudney R <rudney.un2016@gmail.com>
2022-05-06 12:10:10 -0300

    Sent emails

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import nodemailer from 'nodemailer';
 import { prisma } from './prisma';
 
 const port = process.env.PORT || 3333;
@@ -9,6 +10,15 @@ interface Request extends express.Request {
   comment: string;
   screenshot?: string;
 }
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "7f09a335bc01a9",
+    pass: "8d1434d7160324"
+  }
+});
 
 server
   .use(express.json())
@@ -21,6 +31,18 @@ server
         comment,
         screenshot,
       }
+    })
+
+    await transport.sendMail({
+      from: 'Equipe Feedget <oi@feedget.com>',
+      to: 'Rudney Rodrigues <rudney.un2016@gmail.com>',
+      subject: `Novo feedback | ${type}`,
+      html: [
+        `<div style="font-family: sans-serif; font-size: 16px; color: #111">`,
+        `<p>Tipo do feedback: ${type}</p>`,
+        `<p>Comentário do feedback: ${comment}</p>`,
+        `</div>`,
+      ].join('\n'),
     })
 
     return res.status(201).json({
